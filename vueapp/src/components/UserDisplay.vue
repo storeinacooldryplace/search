dbf38bd96be4ab586c171816c74a1a5ab0a8c06c
storeinacooldryplace <150088729+storeinacooldryplace@users.noreply.github.com>
2024-10-16 19:44:17 -0400

    Tweaked design of userdisplay

--- vueapp/src/components/UserDisplay.vue
+++ vueapp/src/components/UserDisplay.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="user-stats">
-    <span class="stat"> 2 hours since last search </span>
-    <span class="stat"> 2.6k visitors</span>
-    <span class="stat"> 33 followers</span>
-
+    <span class="stat">@psychobath</span>
+    <span class="stat"> <b>2 hours </b> since last search </span>
+    <span class="stat"><b>2.6k </b>visitors</span>
+    <span class="stat"><b>33</b> followers</span>
   </div>
 </template>
 
@@ -29,6 +29,5 @@ export default {
 
 .stat {
   color: #333;
-  font-weight: bold;
 }
 </style>
